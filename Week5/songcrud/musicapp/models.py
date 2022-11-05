8762bcef86ae78c3f9819bdcea02781c021df94e
Alade Toheeb <aladetoheeb5@gmail.com>
2022-11-05 07:09:01 +0100

    add relation

--- Week5/songcrud/musicapp/models.py
+++ Week5/songcrud/musicapp/models.py
@@ -7,12 +7,18 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
 class Song(models.Model):
     title = models.CharField(max_length=50)
     date_released = models.DateTimeField()
     likes = models.IntegerField()
-    artise_id = models.IntegerField()
+    # artise_id = models.IntegerField()
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
 
 class Lyric(models.Model):
     content = models.TextField()
-    song_id = models.IntegerField()
+    # song_id = models.IntegerField()
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
